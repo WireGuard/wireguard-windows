802679deed16d081514e1612d09f71ba78e8a20b
Alexander Neumann <an2048@gmail.com>
2012-08-30 17:25:32 +0200

    Add SHGetFileInfo

--- shell32.go
+++ shell32.go
@@ -102,6 +102,28 @@ const (
 
 const NOTIFYICON_VERSION = 3
 
+// SHGetFileInfo flags
+const (
+	SHGFI_LARGEICON         = 0x000000000
+	SHGFI_SMALLICON         = 0x000000001
+	SHGFI_OPENICON          = 0x000000002
+	SHGFI_SHELLICONSIZE     = 0x000000004
+	SHGFI_PIDL              = 0x000000008
+	SHGFI_USEFILEATTRIBUTES = 0x000000010
+	SHGFI_ADDOVERLAYS       = 0x000000020
+	SHGFI_OVERLAYINDEX      = 0x000000040
+	SHGFI_ICON              = 0x000000100
+	SHGFI_DISPLAYNAME       = 0x000000200
+	SHGFI_TYPENAME          = 0x000000400
+	SHGFI_ATTRIBUTES        = 0x000000800
+	SHGFI_ICONLOCATION      = 0x000001000
+	SHGFI_EXETYPE           = 0x000002000
+	SHGFI_SYSICONINDEX      = 0x000004000
+	SHGFI_LINKOVERLAY       = 0x000008000
+	SHGFI_SELECTED          = 0x000010000
+	SHGFI_ATTR_SPECIFIED    = 0x000020000
+)
+
 type NOTIFYICONDATA struct {
 	CbSize           uint32
 	HWnd             HWND
@@ -119,11 +141,20 @@ type NOTIFYICONDATA struct {
 	GuidItem         GUID
 }
 
+type SHFILEINFO struct {
+	HIcon         HICON
+	IIcon         int32
+	DwAttributes  uint32
+	SzDisplayName [MAX_PATH]uint16
+	SzTypeName    [80]uint16
+}
+
 var (
 	// Library
 	libshell32 uintptr
 
 	// Functions
+	sHGetFileInfo          uintptr
 	shGetSpecialFolderPath uintptr
 	shell_NotifyIcon       uintptr
 )
@@ -134,7 +165,20 @@ func init() {
 
 	// Functions
 	shGetSpecialFolderPath = MustGetProcAddress(libshell32, "SHGetSpecialFolderPathW")
+	sHGetFileInfo = MustGetProcAddress(libshell32, "SHGetFileInfoW")
 	shell_NotifyIcon = MustGetProcAddress(libshell32, "Shell_NotifyIconW")
+}
+
+func SHGetFileInfo(pszPath *uint16, dwFileAttributes uint32, psfi *SHFILEINFO, cbFileInfo, uFlags uint32) uintptr {
+	ret, _, _ := syscall.Syscall6(sHGetFileInfo, 5,
+		uintptr(unsafe.Pointer(pszPath)),
+		uintptr(dwFileAttributes),
+		uintptr(unsafe.Pointer(psfi)),
+		uintptr(cbFileInfo),
+		uintptr(uFlags),
+		0)
+
+	return ret
 }
 
 func ShGetSpecialFolderPath(hwndOwner HWND, lpszPath *uint16, csidl CSIDL, fCreate bool) bool {
