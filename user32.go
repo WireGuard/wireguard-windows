3a42285f2a78369f668776fa2226de2f7b9b5eff
Alexander Neumann <an2048@gmail.com>
2013-05-06 13:20:51 +0200

    Add SendInput

--- user32.go
+++ user32.go
@@ -1089,6 +1089,45 @@ const (
 	MONITORINFOF_PRIMARY = 0x1
 )
 
+// INPUT Type
+const (
+	INPUT_MOUSE    = 0
+	INPUT_KEYBOARD = 1
+	INPUT_HARDWARE = 2
+)
+
+// MOUSEINPUT MouseData
+const (
+	XBUTTON1 = 0x0001
+	XBUTTON2 = 0x0002
+)
+
+// MOUSEINPUT DwFlags
+const (
+	MOUSEEVENTF_ABSOLUTE        = 0x8000
+	MOUSEEVENTF_HWHEEL          = 0x1000
+	MOUSEEVENTF_MOVE            = 0x0001
+	MOUSEEVENTF_MOVE_NOCOALESCE = 0x2000
+	MOUSEEVENTF_LEFTDOWN        = 0x0002
+	MOUSEEVENTF_LEFTUP          = 0x0004
+	MOUSEEVENTF_RIGHTDOWN       = 0x0008
+	MOUSEEVENTF_RIGHTUP         = 0x0010
+	MOUSEEVENTF_MIDDLEDOWN      = 0x0020
+	MOUSEEVENTF_MIDDLEUP        = 0x0040
+	MOUSEEVENTF_VIRTUALDESK     = 0x4000
+	MOUSEEVENTF_WHEEL           = 0x0800
+	MOUSEEVENTF_XDOWN           = 0x0080
+	MOUSEEVENTF_XUP             = 0x0100
+)
+
+// KEYBDINPUT DwFlags
+const (
+	KEYEVENTF_EXTENDEDKEY = 0x0001
+	KEYEVENTF_KEYUP       = 0x0002
+	KEYEVENTF_SCANCODE    = 0x0008
+	KEYEVENTF_UNICODE     = 0x0004
+)
+
 type MONITORINFO struct {
 	CbSize    uint32
 	RcMonitor RECT
@@ -1278,6 +1317,46 @@ type ICONINFO struct {
 	HbmColor HBITMAP
 }
 
+type MOUSE_INPUT struct {
+	Type uint32
+	Mi   MOUSEINPUT
+}
+
+type MOUSEINPUT struct {
+	Dx          int32
+	Dy          int32
+	MouseData   uint32
+	DwFlags     uint32
+	Time        uint32
+	DwExtraInfo uintptr
+}
+
+type KEYBD_INPUT struct {
+	Type uint32
+	Ki   KEYBDINPUT
+}
+
+type KEYBDINPUT struct {
+	WVk         uint16
+	WScan       uint16
+	DwFlags     uint32
+	Time        uint32
+	DwExtraInfo uintptr
+	Unused      [8]byte
+}
+
+type HARDWARE_INPUT struct {
+	Type uint32
+	Hi   HARDWAREINPUT
+}
+
+type HARDWAREINPUT struct {
+	UMsg    uint32
+	WParamL uint16
+	WParamH uint16
+	Unused  [16]byte
+}
+
 func GET_X_LPARAM(lp uintptr) int32 {
 	return int32(int16(LOWORD(uint32(lp))))
 }
@@ -1353,6 +1432,7 @@ var (
 	releaseDC               uintptr
 	removeMenu              uintptr
 	screenToClient          uintptr
+	sendInput               uintptr
 	sendMessage             uintptr
 	setActiveWindow         uintptr
 	setCapture              uintptr
@@ -1451,6 +1531,7 @@ func init() {
 	releaseDC = MustGetProcAddress(libuser32, "ReleaseDC")
 	removeMenu = MustGetProcAddress(libuser32, "RemoveMenu")
 	screenToClient = MustGetProcAddress(libuser32, "ScreenToClient")
+	sendInput = MustGetProcAddress(libuser32, "SendInput")
 	sendMessage = MustGetProcAddress(libuser32, "SendMessageW")
 	setActiveWindow = MustGetProcAddress(libuser32, "SetActiveWindow")
 	setCapture = MustGetProcAddress(libuser32, "SetCapture")
@@ -2082,6 +2163,15 @@ func ScreenToClient(hWnd HWND, point *POINT) bool {
 		0)
 
 	return ret != 0
+}
+
+func SendInput(nInputs uint32, pInputs unsafe.Pointer, cbSize int32) uint32 {
+	ret, _, _ := syscall.Syscall(sendInput, 3,
+		uintptr(nInputs),
+		uintptr(pInputs),
+		uintptr(cbSize))
+
+	return uint32(ret)
 }
 
 func SendMessage(hWnd HWND, msg uint32, wParam, lParam uintptr) uintptr {
