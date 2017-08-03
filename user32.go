aa5a1207ac5e8772fd6164f4f99e9cd7040a4b67
Alexander Neumann <alexander.neumann@picos-software.com>
2017-08-03 10:52:42 +0200

    Add WM_NCHITTEST constants

--- user32.go
+++ user32.go
@@ -1222,6 +1222,36 @@ const (
 	DI_NORMAL      = DI_IMAGE | DI_MASK
 )
 
+// WM_NCHITTEST constants
+const (
+	HTBORDER      = 18
+	HTBOTTOM      = 15
+	HTBOTTOMLEFT  = 16
+	HTBOTTOMRIGHT = 17
+	HTCAPTION     = 2
+	HTCLIENT      = 1
+	HTCLOSE       = 20
+	HTERROR       = -2
+	HTGROWBOX     = 4
+	HTHELP        = 21
+	HTHSCROLL     = 6
+	HTLEFT        = 10
+	HTMENU        = 5
+	HTMAXBUTTON   = 9
+	HTMINBUTTON   = 8
+	HTNOWHERE     = 0
+	HTREDUCE      = 8
+	HTRIGHT       = 11
+	HTSIZE        = 4
+	HTSYSMENU     = 3
+	HTTOP         = 12
+	HTTOPLEFT     = 13
+	HTTOPRIGHT    = 14
+	HTTRANSPARENT = -1
+	HTVSCROLL     = 7
+	HTZOOM        = 9
+)
+
 type NMBCDROPDOWN struct {
 	Hdr      NMHDR
 	RcButton RECT
