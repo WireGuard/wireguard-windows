d92283178d3cabe67bc7ee02834ae84f3d54032e
Jason A. Donenfeld <Jason@zx2c4.com>
2019-02-28 08:12:55 +0100

    webview: remove
    We don't want anything resembling a browser engine near this app.

--- form.go
+++ form.go
@@ -451,20 +451,6 @@ func (fb *FormBase) handleKeyDown(msg *win.MSG) bool {
 
 		hwnd = win.GetParent(hwnd)
 	}
-
-	// WebView
-	walkDescendants(fb.window, func(w Window) bool {
-		if webView, ok := w.(*WebView); ok {
-			webViewHWnd := webView.Handle()
-			if webViewHWnd == msg.HWnd || win.IsChild(webViewHWnd, msg.HWnd) {
-				_ret := webView.translateAccelerator(msg)
-				if _ret {
-					ret = _ret
-				}
-			}
-		}
-		return true
-	})
 	return ret
 }
 
