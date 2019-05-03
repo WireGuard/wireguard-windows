f4b0bd49027c833219cc7f357a96d317dccfab2f
Odd Stranne <odd@mullvad.net>
2019-05-03 16:50:42 +0200

    firewall: introduce incomplete untested prototype
    This doesn't support NDP yet, and some major things are still left to be
    decided, but this is the beginning of something that can be debugged
    into shape.
    
    Signed-off-by: Odd Stranne <odd@mullvad.net>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Aleksandar Pesic <peske.nis@gmail.com>

---
+++ service/firewall/mksyscall.go
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: MIT
+ *
+ * Copyright (C) 2019 WireGuard LLC. All Rights Reserved.
+ */
+
+package firewall
+
+//go:generate go run $GOROOT/src/syscall/mksyscall_windows.go -output zsyscall_windows.go syscall_windows.go
