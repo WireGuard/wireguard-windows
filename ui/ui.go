9478bbd46d66579a187810f397afa5244173bb97
Jason A. Donenfeld <Jason@zx2c4.com>
2019-05-12 12:32:09 +0200

    ui: leave it to walk to lock thread
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- ui/ui.go
+++ ui/ui.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"github.com/lxn/walk"
 	"golang.zx2c4.com/wireguard/windows/service"
-	"runtime"
 	"runtime/debug"
 	"time"
 )
@@ -18,8 +17,6 @@ var shouldQuitManagerWhenExiting = false
 var startTime = time.Now()
 
 func RunUI() {
-	runtime.LockOSThread()
-
 	defer func() {
 		if err := recover(); err != nil {
 			walk.MsgBox(nil, "Panic", fmt.Sprint(err, "\n\n", string(debug.Stack())), walk.MsgBoxIconError)
