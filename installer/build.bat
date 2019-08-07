17f2d4837a2d0a7b0ea425be8be1658de6167915
Simon Rozman <simon@rozman.si>
2019-08-07 10:03:56 +0200

    installer: unify WIREGUARD_-specific WiX variables
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- installer/build.bat
+++ installer/build.bat
@@ -63,7 +63,7 @@ if exist .deps\prepared goto :build
 
 :msi
 	echo [+] Compiling %1
-	"%WIX%bin\candle" %WIX_CANDLE_FLAGS% -dPlatform="%~1" -out "%~1\wireguard.wixobj" -arch %2 wireguard.wxs || exit /b %errorlevel%
+	"%WIX%bin\candle" %WIX_CANDLE_FLAGS% -dWIREGUARD_PLATFORM="%~1" -out "%~1\wireguard.wixobj" -arch %2 wireguard.wxs || exit /b %errorlevel%
 	echo [+] Linking %1
 	"%WIX%bin\light" %WIX_LIGHT_FLAGS% -out "dist\wireguard-%~1-%WIREGUARD_VERSION%.msi" "%~1\wireguard.wixobj" || exit /b %errorlevel%
 	goto :eof
