b70b012bc692477fdf51b09627c6a7333265ee31
Jason A. Donenfeld <Jason@zx2c4.com>
2019-08-29 16:16:48 -0600

    version: hard code name and version at compile time
    We really do want the true name and version in logs so that external
    consumers have a good reference point for helping us debug.
    
    We can then do the log file directory explicitly.
    
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- version/mksyscall.go
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: MIT
- *
- * Copyright (C) 2019 WireGuard LLC. All Rights Reserved.
- */
-
-package version
-
-//go:generate go run $GOROOT/src/syscall/mksyscall_windows.go -output zsyscall_windows.go os_windows.go version_windows.go
