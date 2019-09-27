ddc861d8a56edcc3248990b6193e26c2b1ce8810
Jason A. Donenfeld <Jason@zx2c4.com>
2019-09-27 10:42:44 +0200

    Revert "updater: use winhttp to reduce filesize"
    This reverts commit b1a33fd099fdcc25b0edba9c0e3f2ea9f8d0d9c4.
    
    We'll revisit this later.

--- updater/winhttp/mksyscall.go
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: MIT
- *
- * Copyright (C) 2019 WireGuard LLC. All Rights Reserved.
- */
-
-package winhttp
-
-//go:generate go run $GOROOT/src/syscall/mksyscall_windows.go -output zsyscall_windows.go syscall_windows.go
