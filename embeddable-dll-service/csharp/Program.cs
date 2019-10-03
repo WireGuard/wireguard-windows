5c734577227d10b9043c7cf4f63bd3665d515a15
Jason A. Donenfeld <Jason@zx2c4.com>
2019-10-03 16:59:50 +0200

    embeddable-dll-service: add csharp example code

---
+++ embeddable-dll-service/csharp/Program.cs
@@ -0,0 +1,56 @@
+﻿/* SPDX-License-Identifier: MIT
+ *
+ * Copyright (C) 2019 WireGuard LLC. All Rights Reserved.
+ */
+
+using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+
+namespace Tunnel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2 && args[0] == "/service")
+            {
+                Service.Run(args[1]);
+                return;
+            }
+            var keys = Keypair.Generate();
+            var client = new TcpClient("demo.wireguard.com", 42912);
+            var stream = client.GetStream();
+            var reader = new StreamReader(stream, Encoding.UTF8);
+            var pubKeyBytes = Encoding.UTF8.GetBytes(keys.Public + "\n");
+            stream.Write(pubKeyBytes, 0, pubKeyBytes.Length);
+            stream.Flush();
+            var ret = reader.ReadLine().Split(':');
+            client.Close();
+            var status = ret.Length >= 1 ? ret[0] : "";
+            var serverPubkey = ret.Length >= 2 ? ret[1] : "";
+            var serverPort = ret.Length >= 3 ? ret[2] : "";
+            var internalIP = ret.Length >= 4 ? ret[3] : "";
+
+            if (status != "OK")
+                throw new InvalidOperationException(String.Format("Server status is {0}", status));
+
+            var configFileContents = String.Format("[Interface]\nPrivateKey = {0}\nAddress = {1}/24\nDNS = 8.8.8.8, 8.8.4.4\n\n[Peer]\nPublicKey = {2}\nEndpoint = demo.wireguard.com:{3}\nAllowedIPs = 0.0.0.0/0\n", keys.Private, internalIP, serverPubkey, serverPort);
+            var configFile = Path.Combine(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName), "demobox.conf");
+            File.WriteAllText(configFile, configFileContents);
+
+            try
+            {
+                Service.Add(configFile);
+                Console.WriteLine("=== Press enter to exit ===");
+                Console.ReadLine();
+            }
+            finally
+            {
+                Service.Remove(configFile);
+            }
+        }
+    }
+}
---
+++ embeddable-dll-service/csharp/Win32.cs
@@ -0,0 +1,245 @@
+﻿/* SPDX-License-Identifier: MIT
+ *
+ * Copyright (C) 2019 WireGuard LLC. All Rights Reserved.
+ */
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Tunnel
+{
+    static class Win32
+    {
+        [Flags]
+        public enum ScmAccessRights
+        {
+            Connect = 0x0001,
+            CreateService = 0x0002,
+            EnumerateService = 0x0004,
+            Lock = 0x0008,
+            QueryLockStatus = 0x0010,
+            ModifyBootConfig = 0x0020,
+            StandardRightsRequired = 0xF0000,
+            AllAccess = (StandardRightsRequired | Connect | CreateService | EnumerateService | Lock | QueryLockStatus | ModifyBootConfig)
+        }
+
+        [Flags]
+        public enum ServiceAccessRights
+        {
+            QueryConfig = 0x1,
+            ChangeConfig = 0x2,
+            QueryStatus = 0x4,
+            EnumerateDependants = 0x8,
+            Start = 0x10,
+            Stop = 0x20,
+            PauseContinue = 0x40,
+            Interrogate = 0x80,
+            UserDefinedControl = 0x100,
+            Delete = 0x00010000,
+            StandardRightsRequired = 0xF0000,
+            AllAccess = (StandardRightsRequired | QueryConfig | ChangeConfig | QueryStatus | EnumerateDependants | Start | Stop | PauseContinue | Interrogate | UserDefinedControl)
+        }
+
+        [Flags]
+        public enum ServiceStartType
+        {
+            Boot = 0x00000000,
+            System = 0x00000001,
+            Auto = 0x00000002,
+            Demand = 0x00000003,
+            Disabled = 0x00000004
+        }
+
+        [Flags]
+        public enum ServiceControl
+        {
+            Stop = 0x00000001,
+            Pause = 0x00000002,
+            Continue = 0x00000003,
+            Interrogate = 0x00000004,
+            Shutdown = 0x00000005,
+            ParamChange = 0x00000006,
+            NetBindAdd = 0x00000007,
+            NetBindRemove = 0x00000008,
+            NetBindEnable = 0x00000009,
+            NetBindDisable = 0x0000000A
+        }
+
+        [Flags]
+        public enum ServiceError
+        {
+            Ignore = 0x00000000,
+            Normal = 0x00000001,
+            Severe = 0x00000002,
+            Critical = 0x00000003
+        }
+
+        [Flags]
+        public enum ServiceSidType
+        {
+            None = 0x00000000,
+            Unrestricted = 0x00000001,
+            Restricted = 0x00000003
+        }
+
+        [Flags]
+        public enum ServiceType
+        {
+            KernelDriver = 0x00000001,
+            FileSystemDriver = 0x00000002,
+            Win32OwnProcess = 0x00000010,
+            Win32ShareProcess = 0x00000020,
+            InteractiveProcess = 0x00000100
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode, Size = 8192), ComVisible(false)]
+        public struct ServiceSidInfo
+        {
+            public ServiceSidType serviceSidType;
+        };
+
+        public enum ServiceState
+        {
+            Unknown = -1,
+            NotFound = 0,
+            Stopped = 1,
+            StartPending = 2,
+            StopPending = 3,
+            Running = 4,
+            ContinuePending = 5,
+            PausePending = 6,
+            Paused = 7
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class ServiceStatus
+        {
+            public int dwServiceType = 0;
+            public ServiceState dwCurrentState = 0;
+            public int dwControlsAccepted = 0;
+            public int dwWin32ExitCode = 0;
+            public int dwServiceSpecificExitCode = 0;
+            public int dwCheckPoint = 0;
+            public int dwWaitHint = 0;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode, Size = 8192), ComVisible(false)]
+        public struct ServiceDescription
+        {
+            public String lpDescription;
+        };
+
+        public enum ServiceConfigType
+        {
+            Description = 1,
+            SidInfo = 5
+        }
+
+        [DllImport("advapi32.dll", EntryPoint = "OpenSCManagerW", ExactSpelling = true, CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern IntPtr OpenSCManager(string machineName, string databaseName, ScmAccessRights dwDesiredAccess);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr OpenService(IntPtr hSCManager, string lpServiceName, ServiceAccessRights dwDesiredAccess);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseServiceHandle(IntPtr hSCObject);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool StartService(IntPtr hService, int dwNumServiceArgs, string[] lpServiceArgVectors);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr CreateService(IntPtr hSCManager, string lpServiceName, string lpDisplayName, ServiceAccessRights dwDesiredAccess, ServiceType dwServiceType, ServiceStartType dwStartType, ServiceError dwErrorControl, string lpBinaryPathName, string lpLoadOrderGroup, IntPtr lpdwTagId, string lpDependencies, string lp, string lpPassword);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteService(IntPtr hService);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool ControlService(IntPtr hService, ServiceControl dwControl, ServiceStatus lpServiceStatus);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool QueryServiceStatus(IntPtr hService, ServiceStatus lpServiceStatus);
+
+        [DllImport("advapi32.dll", EntryPoint = "ChangeServiceConfig2", CharSet = CharSet.Unicode, SetLastError = true, CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool ChangeServiceConfig2(IntPtr hService, ServiceConfigType dwInfoLevel, ref ServiceSidType lpInfo);
+
+        [DllImport("advapi32.dll", EntryPoint = "ChangeServiceConfig2", CharSet = CharSet.Unicode, SetLastError = true, CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool ChangeServiceConfig2(IntPtr hService, ServiceConfigType dwInfoLevel, ref ServiceDescription lpInfo);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        public class KeyBlob
+        {
+            BCRYPT_ECCKEY_BLOB Header;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+            public byte[] Public;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+            public byte[] Unused;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+            public byte[] Private;
+        }
+
+        public const string BCRYPT_ECC_CURVE_NAME = "ECCCurveName";
+        public const string BCRYPT_ECDH_ALGORITHM = "ECDH";
+        public const string BCRYPT_ECC_CURVE_25519 = "curve25519";
+        public const string BCRYPT_ECCPRIVATE_BLOB = "ECCPRIVATEBLOB";
+
+        [DllImport("bcrypt.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        public static extern uint BCryptSetProperty(IntPtr hObject, string property, string input, int inputSize, uint Flags = 0);
+
+        [DllImport("bcrypt.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern uint BCryptOpenAlgorithmProvider(ref IntPtr hAlgorithm, string AlgId, string Implementation, uint Flags);
+
+        [DllImport("bcrypt.dll", SetLastError = true)]
+        public static extern uint BCryptGenerateKeyPair(IntPtr hObject, ref IntPtr hKey, uint length, uint Flags);
+
+        [DllImport("bcrypt.dll", SetLastError = true)]
+        public static extern uint BCryptFinalizeKeyPair(IntPtr hKey, uint Flags);
+
+        [DllImport("bcrypt.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        public static extern uint BCryptExportKey(IntPtr hKey, IntPtr hExportKey, [MarshalAs(UnmanagedType.LPWStr)] string pszBlobType, [Out] KeyBlob pbOutput, int cbOutput, out int pcbResult, uint Flags = 0);
+
+        [DllImport("bcrypt.dll", SetLastError = true)]
+        public static extern uint BCryptDestroyKey(IntPtr hKey);
+
+        [DllImport("bcrypt.dll", SetLastError = true)]
+        public static extern uint BCryptCloseAlgorithmProvider(IntPtr hAlgorithm, uint Flags);
+
+        [DllImport("bcrypt.dll", SetLastError = true)]
+        public static extern uint BCryptDestroySecret(IntPtr hSecretAgreement);
+
+        [DllImport("bcrypt.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern uint BCryptImportKeyPair(IntPtr hAlgorithm, IntPtr hImportKey, string BlobType, ref IntPtr hPublicKey, byte[] Input, uint InputByteLength, uint Flags);
+
+        [DllImport("bcrypt.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern uint BCryptSecretAgreement(IntPtr hPrivKey, IntPtr hPublicKey, ref IntPtr phSecret, uint Flags);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class BCryptBufferDesc
+        {
+            public uint ulVersion;
+            public uint cBuffers;
+            public IntPtr pBuffers;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class BCryptBuffer
+        {
+            public uint cbBuffer;
+            public uint bufferType;
+            public IntPtr pvBuffer;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class BCRYPT_ECCKEY_BLOB
+        {
+            uint magic;
+            uint cbKey;
+        }
+    }
+}
