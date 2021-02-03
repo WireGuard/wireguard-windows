83d447139eb246f787f33215aa9edd54aa61b847
Jason A. Donenfeld <Jason@zx2c4.com>
2021-02-03 23:57:23 +0100

    Port to arm64
    Updates: https://github.com/golang/go/issues/36439
    Updates: https://github.com/golang/go/issues/44020
    
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- shobj_64.go
+++ shobj_64.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build windows,amd64
+// +build windows,amd64 windows,arm64
 
 package win
 
