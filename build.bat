e36d3736827dd3e1e3112fc1b47f7d567ee5c623
Jason A. Donenfeld <Jason@zx2c4.com>
2021-11-25 18:18:09 +0100

    build: switch to go.dev
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- build.bat
+++ build.bat
@@ -13,7 +13,7 @@ if exist .deps\prepared goto :render
 	rmdir /s /q .deps 2> NUL
 	mkdir .deps || goto :error
 	cd .deps || goto :error
-	call :download go.zip https://golang.org/dl/go1.17.3.windows-amd64.zip e78684b955742e215926204afc6ed62b9d165b509e25a687d62902516f08726b || goto :error
+	call :download go.zip https://go.dev/dl/go1.17.3.windows-amd64.zip e78684b955742e215926204afc6ed62b9d165b509e25a687d62902516f08726b || goto :error
 	rem Mirror of https://github.com/mstorsjo/llvm-mingw/releases/download/20201020/llvm-mingw-20201020-msvcrt-x86_64.zip
 	call :download llvm-mingw-msvcrt.zip https://download.wireguard.com/windows-toolchain/distfiles/llvm-mingw-20201020-msvcrt-x86_64.zip 2e46593245090df96d15e360e092f0b62b97e93866e0162dca7f93b16722b844 || goto :error
 	rem Mirror of https://imagemagick.org/download/binaries/ImageMagick-7.0.8-42-portable-Q16-x64.zip
